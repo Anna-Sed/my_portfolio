554b071e98f6550ff5608ba4c37ff1511f0579d9
Anna-Sed <79031226390a@gmail.com>
2026-09-04 00:19:02 +0300

    Import new component Navbar to src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,13 @@
 import { useState } from 'react'
 import "./styles/main.css"
-import sun from './img/icons/sun.svg'
-import moon from './img/icons/moon.svg'
+import Navbart from './components/navbar/Navbar'
 import github from './img/icons/gitHub.svg'
 import tg from './img/icons/tg.png'
 
 function App() {
   return (
     <div className='App'> 
-      <nav className='nav'>
-        <div className="container">
-          <div className="nav-row">
-            <a className='logo' href='#'><strong>My</strong> portfolio</a> 
-            <button className="dark-mode-btn">
-              <img src={sun} alt="Light mode" className="dark-mode-btn__icon"></img>
-              <img src={moon} alt="Dark mode" className="dark-mode-btn__icon"></img>
-            </button>
-            <ul className="nav-list">
-              <li className="nav-list__item"><a href="./index.html" className="nav-list__link nav-list__link--active">Home</a></li>
-              <li className="nav-list__item"><a href="./projects.html" className="nav-list__link">Projects</a></li>
-              <li className="nav-list__item"><a href="./contacts.html" className="nav-list__link">Contacts</a></li>
-            </ul>
-          </div>
-        </div>
-      </nav>
+      <Navbart />
 
       <header className='header'>
         <div className="header__wrapper">
