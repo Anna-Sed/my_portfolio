4ba6b0713692b4d4624bb7c44827ff38df14af7c
Anna-Sed <79031226390a@gmail.com>
2026-09-06 22:45:18 +0300

    Move the Header-component and main-section in a new Home-component

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,16 @@
 import { useState } from 'react'
 import "./styles/main.css"
 import Navbart from './components/navbar/Navbar'
-import Header from './components/header/Header'
 import Footer from './components/footer/Footer'
+import Home from './pages/home'
 
 
 function App() {
   return (
     <div className='App'> 
       <Navbart />
-      <Header />
+      <Home />
       <Footer />
-
-      <main className="section">
-        <div className="container">
-          <ul className="content-list">
-            <li className="content-list__item">
-              <h2 className="title-2">Frontend</h2>
-              <p>JavaScript, TypeScript, ReactJS, Redux, HTML, CSS, NPM, BootStrap</p>
-            </li>
-            <li className="content-list__item">
-              <h2 className="title-2">Backend</h2>
-              <p>NodeJS</p>
-            </li>
-          </ul>
-        </div>
-      </main>
 
     </div>
   )
